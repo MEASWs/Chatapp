92ec04f9424fad276b97fcc61885e4c66eaf0095
MEASWs <mookvarin.cc1976@gmail.com>
2026-02-09 21:14:45 +0700

    fix: localhost is back

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,32 @@
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      process.env.CLIENT_URL, // 👈 Vercel URL
-    ],
+import express from "express"   // import express for server
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js" // import message routes
+// import friendRoutes from "./routes/friend.route.js"; // ใช้ friend.route.js
+import dotenv from "dotenv"             // load env variables
+import { connectDB } from "./lib/db.js";        // import db connection function
+import cookieParser from "cookie-parser"; // import cookie parser for cookies
+import cors from "cors"; // import cors for cross-origin resource sharing
+import { app,server } from "./lib/socket.js";
+
+
+dotenv.config()     // load env variables
+
+const PORT = process.env.PORT // set port      
+
+app.use(express.json()) // for parsing application/json
+app.use(cookieParser()) // for parsing cookies
+app.use(cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
-  })
+})
 )
+
+
+app.use("/api/auth", authRoutes)    // for auth routes
+app.use("/api/messages", messageRoutes)    // for message routes
+// app.use("/api/friends", friendRoutes); // for friend routes
+
+server.listen(PORT,() => {     // start server  
+    console.log("server run PORT:" + PORT)
+    connectDB()
+})
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
-  withCredentials: true,
+    baseURL: "http://localhost:5001/api",
+    withCredentials: true,
 });
